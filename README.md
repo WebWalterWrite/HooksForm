755a235491bc07bd3fb2d80683ef8a3e293f7cbe
Sammy el dueno <sammyeldueno@imac-de-sammy.home>
2018-11-07 23:40:45 +0100

    type:feat(user) description: ajout fonction onSubmit relative au formulaire

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Premiers pas avec React Hooks
+<img alt="react hooks" src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Nosehook-drawing-bw.png" height="500px"/></div>
 
-![hooks man](https://upload.wikimedia.org/wikipedia/commons/7/7a/Nosehook-drawing-bw.png)
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 
+// State
 const useInputValue = (initialValue, type, placeholder) => {
   const [value, setValue] = useState(initialValue);
 
@@ -10,29 +11,45 @@ const useInputValue = (initialValue, type, placeholder) => {
       e.preventDefault();
       setValue(e.target.value);
     },
-      value,
-      type,
-      placeholder,
+    value,
+    type,
+    placeholder,
+    name: placeholder,
   };
 };
 
+// Soumission du formulaire
+const onSubmit = e => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+    let user = {
+    firstname : data.get('firstname'),
+    lastname :data.get('lastname'),
+    email :data.get('email'),
+    }
+
+    console.log(user)
+};
+
+
+// Formulaire
 export const Form = () => {
+
   const firstname = useInputValue("", "text", "firstname");
   const lastname = useInputValue("", "text", "lastname");
-  const email = useInputValue("", "email", "email adress");
+  const email = useInputValue("", "email", "email");
+
 
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
+              <label htmlFor="firstname">Enter username</label>
         <input {...firstname} />
         <input {...lastname} />
         <input {...email} />
+        <input type="submit" value="valider" />
       </form>
     </div>
   );
 };
-
-
-
-
-
