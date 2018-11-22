ae5f4b4f6463dbb0e914abe47eec0e991f0f51c5
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-11-22 22:33:56 +0100

    type:refactor(validation) desc:mise à jour des messages d'erreurs et de success

--- src/components/form/UserPwdForgot.js
+++ src/components/form/UserPwdForgot.js
@@ -1,23 +1,47 @@
-import React, { useState } from "react";
-import { useInputValue } from '../common/useState';
+import React, { Fragment, useState } from "react";
+import { useInputValue } from "../common/useState";
 import { fetchForm } from "../../utils/api";
+import { isEmail } from '../../utils/form-validation';
 
-export const userPwdForgot = () => {
+const errStyle = {color:'#FF5252', fontSize:'0.8em'}
+const succStyle = { color: "#66BB6A", fontSize: "0.8em" };
 
-    const [msg, setMsg] = useState({});
+export const UserPwdForgot = () => {
+	const [msg, setMsg] = useState({});
 
-    const onSubmit = e => {
-        e.preventDefault();
-        const data = new FormData (e.target);
-
-        let userEmail = data.get('email'); 
-
-        getEmail (userEmail);
-    }
-
-    const getEmail = async data => {
-
-        const result  = await fetchForm(data)
-        setMsg(result)
-    }
+	// soumission du formulaire
+	const onSubmit = e => {
+        setMsg({})
+		e.preventDefault();
+		const data = new FormData(e.target);
+		let userEmail = {email: data.get("email")};
+		resClient(userEmail);
+    };
+    
+	// Valider côté client le formulaire.
+	const resClient = async data => {
+		const result = await isEmail(data);
+		result ? setMsg(result) : resServer(data)
+    };
+    
+    // Envoyer vers serveur la demande
+    const resServer = async data => {
+        const result = await fetchForm(data,'forgot');
+        return setMsg(result);
+    };
+    console.log(msg)
+	const email = useInputValue("", "email", "ex: cersei@portreal.got", "email");
+	return (
+		<Fragment>  
+			<form onSubmit={onSubmit}>
+				{/* input email */}
+				<div>
+					<label htmlFor="email">Email</label>
+					<input {...email} />
+					<div style={!msg.email ? errStyle : succStyle}>{msg.email || msg.noEmail}</div>
+				</div>
+				<input type="submit" value="valider" />
+			</form>
+		</Fragment>
+	);
 };
