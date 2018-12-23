03666e5649886479cc3d68ffcbe0c5ed9f693211
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-12-23 15:13:03 +0100

    type:applicatif(routing) desc: ajout route UserConnexion

--- src/Routing.js
+++ src/Routing.js
@@ -1,5 +1,6 @@
 import { UserCreate } from "./components/form/UserCreate";
 import { UserPwdForgot } from "./components/form/UserPwdForgot";
+import { UserConnexion } from './components/form/UserConnexion';
 
 const AllRoutes = [
 	{
@@ -9,7 +10,11 @@ const AllRoutes = [
 	{
 		path: "/formulaire/mot-de-passe-oublie",
 		component: UserPwdForgot
-	}
+	},
+	{
+		path: "/formulaire/connexion",
+		component: UserConnexion
+	},
 ];
 
 export default AllRoutes;
