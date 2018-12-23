d1e6cdb83e375aa077299035d033f5695054317a
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-12-23 15:21:18 +0100

    type:content

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Intro, Nav } from './layout.styled';
 const Header = () => (
     <header>
+        <Intro>
         <h1>Hooks Form</h1>
-        <nav>
+        <p>Quelques formulaires réalisés avec la nouvelle fonctionnalité <strong>useState</strong> de React. Ils sont fonctionnels côté serveur, testez-les et vous recevrez un email expédié depuis un back conçu en NodeJs, Nodemailer, nodemailer-express-handlebars, GMAIL API</p>
+        </Intro>
+        <Nav>
             <ul>
                 <li>
                     <Link to="/formulaire/inscription">S'inscire</Link>
@@ -20,7 +23,7 @@ const Header = () => (
                     <Link to="/formulaire/reinitialisation-mot-de-passe">Nouveau mot de passe</Link>
                 </li>
             </ul>
-        </nav>
+        </Nav>
     </header>
 )
 
