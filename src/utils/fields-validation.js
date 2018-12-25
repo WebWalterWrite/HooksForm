b36f534b7155fe4b4f2c654f3923f1db38ad3246
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-12-25 23:28:00 +0100

    type: functionnel desc: ajout function validation de mot de passe ( regex: chiffres, Lettres et caractères)

--- src/utils/fields-validation.js
+++ src/utils/fields-validation.js
@@ -1,6 +1,14 @@
 // Objet contenant les éventuelles erreurs
 let msg;
 
+
+
+/**
+ * @func isEmpty - Vérifie si champ n'est pas vide et transfére l'action à la prochaine fonction
+ * @param {*} val  - Contient la saisie du champ input
+ * @param {*} field - Contient le nom du champ
+ * @returns {Function | string} - passe à la prochaine fonction ou renvoi un msg d'erreur
+ */
 const isEmpty = (val, field) => {
     msg = `${field === 'email' ? "L'" : 'Le'} ${field} doit être rempli`;
     return val.length === 0
@@ -10,24 +18,90 @@ const isEmpty = (val, field) => {
     : isEmail(val, field)
 };
 
+
+
+/**
+ * @func isLength - Test la longueur du champ 
+ * @param {string} val - Contient la saisie du champ input
+ * @param {string} field - contient le nom du champ
+ * @return {boolean | string } - Si aucune erreur ou  un msg avec l'erreur rencontrée
+ */
 export const isLength = (val, field) => {
     msg = `Le ${field} doit contenir entre 3 et 30 caractères`;
-
+   
     const size = 2 < val.length && val.length < 32
-    return !size ? msg : false
+    return !size 
+    ? msg
+    : field !== 'mot de passe'
+    ? false
+    : isPassword(val, field)
 };
 
+
+
+/**
+ * @desc Valider le format du mot de passe
+ * @func isPassword 
+ * @param {string} val - Contient la saisie du champ input
+ * @param {string} field - contient le nom du champ
+ * @returns { boolean | object } - Si aucune erreur ou un objet contenant les erreurs.
+ */
+export const isPassword = async (val, field) =>{
+
+    // Conteneur errurs
+    const errors = {};
+
+    // Message erreur
+    msg = `Le ${field} doit contenir au moins`
+    const specialsCharacter = "un des caractères $, @, !, %, ?";
+    const upperCase = "une lettre majuscule";
+    const number = "2 chiffres consécutifs entre 0 et 9";
+
+    // Regex
+    const regexNumeric = /(?:(?=.*[0-9])(?=.*\d{2}).*)/;
+    const regexUpperCase = /(?=.*[A-Z])/
+    const regexSpecCharacters = /(?=.*[$@!%])/
+
+    // Vérifications condition
+    const isNumber = regexNumeric.test(val);
+    const isUpperCase =regexUpperCase.test(val);
+    const isSpecCharacters = regexSpecCharacters.test(val);
+
+    // Injecter les erreurs si false
+    errors.number = !isNumber && `${msg} ${number}`;
+    errors.upperCase = !isUpperCase && `${msg} ${upperCase}`;
+    errors.specCharacter = !isSpecCharacters && `${msg} ${specialsCharacter}`;
+    
+    // Retourner les erreurs
+    return await isErrors([errors]);
+
+}
+
+
+
+/**
+ * @func isEmail - Vérifie que le champ email contient bien un email.
+ * @param {string} val - Contient la saisie du champ input.
+ * @param {string} field  -Contient le nom du champ.
+ * @returns {string | boolean} - Retounrne message erreur ou false. 
+ */
 export const isEmail = (val, field) => {
-    console.log(val)
     msg = `Format ${field} invalide`;
     return !val.includes('@') ? msg : false
-
 };
-// Supprimer les champs sans erreurs
-const isErrors = (errors) => {
+
+
+
+/**
+ * @func isErrors - Vérifie et retourne les erreurs
+ * @param {object} errors - contient les erreurs de saisie du formulaire 
+ * @returns {Array | boolean} - Renvoi un tableau d'objet avec les erreurs ou false si aucune erreur.
+ */
+ const isErrors = (errors) => {
     for(let error in errors){
         if(!errors[error]) delete (errors[error])
     }   
+   
     return Object.values(errors).length > 0 ? errors : false
 };
 export {
