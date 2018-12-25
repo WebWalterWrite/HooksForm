5a9f2f8e72595d37630d9dee65e8c52cd14eb7a6
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-12-25 23:22:39 +0100

    type:style desc: modif style erreurs et affichage responsive

--- src/components/form/form.styled.js
+++ src/components/form/form.styled.js
@@ -1,57 +1,68 @@
-import styled from 'styled-components';
-import {media} from '../../utils/responsive.styled';
+import styled from "styled-components";
+import { media } from "../../utils/responsive.styled";
 
-export const Title= styled.h1`
-      border:solid 2px #61dafb;
-      color:#61dafb;
-      text-align:center;
-      margin: 0 auto;
-      margin-top:2rem;
-      padding:.8rem;
-      width:20%;
-      ${media.phone`
+export const Title = styled.h1`
+	border: solid 2px #61dafb;
+	color: #61dafb;
+	text-align: center;
+	margin: 0 auto;
+	padding: 1rem;
+	width: 20%;
+	${media.phone`
             width:70%;
           `}
 `;
 export const Form = styled.form`
-  text-align:center;
-  margin-top:1rem;
-  label{
-      color:#FFFFFF;
-      font-size:3vh;
-
-  }
-    button{
-          background-color:#CF2D53;
-          color:#FFFFFF;
-          border-radius:none !important;
-          font-size:3vh;
-          padding:.5rem;
-          width:10%;
-          outline:none;
-          ${media.phone`
+	text-align: center;
+	margin-top: 1rem;
+      padding-bottom:1rem;
+      height:auto;
+	label {
+		color: #ffffff;
+		font-size: 3vh;
+	}
+	button {
+		background-color: #cf2d53;
+		color: #ffffff;
+		border-radius: none !important;
+		font-size: 3vh;
+		padding: 0.5rem;
+            margin:1rem 0 0 0;
+		width: 10%;
+		outline: none;
+		${media.phone`
             width:30%;
           `}
-      }
+	}
 `;
 
 export const Input = styled.input`
-      font-size:3vh;
-      outline-color:#61dafb;
-      padding:1vh;
-      width:20%;
-      margin-bottom:1.5vh;
-      border-radius:0;
-      background-color:${props => props.color};
-      ${media.phone`
+	font-size: 3vh;
+	outline-color: #61dafb;
+	padding: 1vh;
+	width: 20%;
+	margin-bottom: 1.5vh;
+	border-radius: 0;
+	background-color: ${props => props.color};
+	${media.phone`
             width:70%;
           `}
-      
-`
+`;
 
 export const Error = styled.div`
-      color:${props => props.errStyle && '#FF5252'};
-      font-size:1em;
-      height:1em;
-      margin-bottom:1em;
+	color: ${props => props.errStyle && "#F05B24"};
+	padding: ${props => props.errStyle ? .5 + "rem" : 0};
+	font-size: 1em;
+	margin: 0 auto;
+	width: 20%;
+	${media.phone`
+            width: 70%;
+      `}
+	>ul {
+		margin: 0 auto;
+		text-align: left;
+		li {
+			margin-left: 2em;
+		}
+	}
 `;
