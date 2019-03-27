a871b327362a3b1b7c32a3f51d5b7bfac39eb87c
WebWalterWrite <webwalterwhrite@gmail.com>
2019-03-27 10:39:11 +0100

    type: refactor desc: delete isEmail fn / add 3eme parametre à la fn isValidForm qui contient le type de test

--- src/utils/form-validation.js
+++ src/utils/form-validation.js
@@ -8,30 +8,26 @@ import {isEmpty, isErrors} from './fields-validation';
 @returns {Object | Boolean } Objet contenant les erreurs ou false si aucune erreur.
 */
 export const isValidForm = async (data) => {
-    
     const errors = {};
-    const {firstname, lastname, email, password, repassword} = data;
 
-    errors.errFirstname = firstname !== undefined && await isEmpty(firstname, 'prenom');
-    errors.errLastname = lastname  !== undefined && await isEmpty(lastname,'nom');
-    errors.errEmail = email !== undefined && await isEmpty(email, 'email');
-    errors.errPassword = password !== undefined && await isEmpty(password, 'mot de passe');
-    errors.errRepassword = repassword !== undefined && await isEmpty(repassword, "mot de passe");
+    const {
+        firstname:f="",
+        lastname:l="",
+        email:e="",
+        password:p="",
+        repassword:rp=""
+    } = data;
+
+    errors.errFirstname = f.val !== undefined && await isEmpty(f.val, 'prenom', f.type);
+    errors.errLastname = l.val  !== undefined && await isEmpty(l.val,'nom', l.type);
+    errors.errEmail = e.val !== undefined && await isEmpty(e.val, 'email', e.type);
+    errors.errPassword = p.val !== undefined && await isEmpty(p.val, 'mot de passe', p.type);
+    errors.errRepassword = rp.val && await isEmpty(rp.val, "mot de passe", rp.type);
 
     return await isErrors(errors);
 }
 
-/**
- * @func isEmail - Vérifier le format email
- * @param {string} user - Contient la saisie du formulaire.
- * @returns {Object | Boolean } Objet contenant les erreurs ou false si aucune erreur.
- */
-export const isEmail = async user => {
-    const errors = {}
-    errors.email = await isEmpty(user.email,'email');
 
-    return await isErrors(errors);
-};
 
 
 
